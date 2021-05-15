bb3b56eb0a78b78cb8edb69b26f7d9893e189e0f
Matt <matt.breaux.dev@gmail.com>
2021-05-14 22:15:23 -0500

    improved the Modal functionality

--- src/components/BookContent.js
+++ src/components/BookContent.js
@@ -7,7 +7,7 @@ function BookContent({ selectedItem }) {
   return (
     <div className="modal-content-container">
       <div className="title-releaseddate-container">
-        <h2>{selectedItem.name}</h2>
+        <h2 className="modal-booktitle">{selectedItem.name}</h2>
         <h4>Release Date: {selectedItem.released}</h4>
       </div>
       <div>
--- src/components/CharactersPerBook.js
+++ src/components/CharactersPerBook.js
@@ -3,7 +3,7 @@ console.log("rendered CharactersPerBook");
 class CharactersPerBook extends React.Component {
   state = {
     listOfCharacters: [],
-    updateCharacters: false,
+    name: "",
   };
   //loop over the characters array of a book and
   //create a promise to fetch data of that character
@@ -13,13 +13,37 @@ class CharactersPerBook extends React.Component {
 
   async componentDidMount() {
     const { selectedItem } = this.props;
-    const characterData = await Promise.all(
-      selectedItem.characters.map((char) =>
-        fetch(char).then((res) => res.json())
-      )
-    );
-    console.log(characterData);
-    this.setState({ listOfCharacters: characterData });
+    console.log({ selectedItem });
+    console.log("name", this.state.name);
+    if (selectedItem.name !== this.state.name) {
+      const characterData = await Promise.all(
+        selectedItem.characters.map((char) =>
+          fetch(char).then((res) => res.json())
+        )
+      );
+      console.log(characterData);
+      this.setState({
+        listOfCharacters: characterData,
+        name: this.props.selectedItem.name,
+      });
+    }
+  }
+  async componentDidUpdate(prevProps) {
+    const { selectedItem } = this.props;
+    console.log({ selectedItem });
+    console.log("name", this.state.name);
+    if (prevProps.selectedItem.name !== this.props.selectedItem.name) {
+      const characterData = await Promise.all(
+        selectedItem.characters.map((char) =>
+          fetch(char).then((res) => res.json())
+        )
+      );
+      console.log(characterData);
+      this.setState({
+        listOfCharacters: characterData,
+        name: this.props.selectedItem.name,
+      });
+    }
   }
 
   render() {
@@ -27,9 +51,12 @@ class CharactersPerBook extends React.Component {
     console.log(listOfCharacters);
     return (
       <div className="characters-names-container">
-        {listOfCharacters.map((char) => (
-          <p className="characters-names">{char.name}</p>
-        ))}
+        {listOfCharacters.length > 0 &&
+          listOfCharacters.map((char, index) => (
+            <p key={index} className="characters-names">
+              {char.name}
+            </p>
+          ))}
       </div>
     );
   }
